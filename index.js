dc805aeb5100a30e644191659d46a74ad1293299
Adedeji Yusuf <adedejiyusuf26@gmail.com>
2021-05-13 18:15:36 +0100

    added

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use("/data", dataRoutes);
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
